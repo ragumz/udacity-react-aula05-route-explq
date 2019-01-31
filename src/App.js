db20cde01c5ac2921a0d2868869b42e01bb0f877
ragumz <ragumz@gmail.com>
2019-01-31 21:01:45 -0200

    3rd Slack's #explain channel exercise from React Route classroom 05 conclusion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom'
 
+const ContactEdit = ({ match }) => (
+  <div>Editing {match.params.username} ...</div>
+)
+const ContactDelete = ({ match }) => (
+  <div>Deleting {match.params.username} ...done!</div>  //--> Nested Route param consume
+)
+const ContactProfile = ({ match }) => (
+  <div>
+    <br></br>
+    <span>Contact Profile of {match.params.username}</span><br></br>
+    <Link to={`${match.url}/edit`}>Edit Contact</Link><br></br>
+    <Link to={`${match.url}/delete`}>Delete Contact</Link>
+    <Route path={`${match.path}/edit`} component={ContactEdit} />
+    <Route path={`${match.path}/delete`} component={ContactDelete} />
+  </div>
+)
+class ListContacts extends Component {
+  render() {
+    return (
+      <div>
+        <ol>
+        {this.props.contacts.map((contact,index) => (
+          <li key={`i${index}`}>
+            <Link key={`l${index}`} 
+                  to={`${this.props.match.url}/${contact.username}`}>{contact.name}</Link>  {/*--> Neste Link declaration */}
+          </li>
+        ))}
+        </ol>
+        <Route path={`${this.props.match.path}/:username`} component={ContactProfile} />  {/*--> (2) Nested Route declaration */}
+      </div>
+    )
+  }
+}
 class App extends Component {
+  state = {
+    contacts: [{ username: 'JHDOE', name: 'Jonh Doe' },
+    { username: 'JNDOE', name: 'Jane Doe' },
+    { username: 'NBDY', name: 'Nobody' }]
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <br></br>
+          <Link to="/list">List Contacts</Link>
+          <Route path="/list"
+            render={(props) => 
+                      (<ListContacts {...props} contacts={this.state.contacts} />) /*--> (1) Pass props to a component rendered by Route */
+                   }
           >
-            Learn React
-          </a>
+          </Route>
         </header>
       </div>
     );
